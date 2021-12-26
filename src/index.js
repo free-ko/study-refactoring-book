6395a72f91b9165304c0b8e32f24f2b3e7726fad
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-26 19:43:41 +0900

    Add : 결과 보일 수 있도록 코드 수정

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import statement from "./statement/statement.js";
+
+function fetchInvoices() {
+  return new Promise((resolve) => {
+    fetch("../data/invoices.json").then((data) => {
+      resolve(data.json());
+    });
+  });
+}
+
+function fetchPlays() {
+  return new Promise((resolve) => {
+    fetch("../data/play.json").then((data) => {
+      resolve(data.json());
+    });
+  });
+}
+
+const invoiceData = await fetchInvoices();
+const playsData = await fetchPlays();
+console.log(statement(invoiceData, playsData));
--- src/statement/statement.js
+++ src/statement/statement.js
@@ -1,15 +1,15 @@
 export default function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `청구 내역(고객명 : ${invoice.customer})\n`;
+  let result = `청구 내역(고객명 : ${invoice[0].customer})\n`;
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
 
-  for (let perf of invoice.performances) {
-    const play = plays[pref.playID];
+  for (const perf of invoice[0].performance) {
+    const play = plays[perf.playID];
     let thisAmount = 0;
 
     switch (play.type) {
