743175a2b8f0c1cc57170e263ab10428711ecf42
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-28 07:46:13 +0900

    [P.35] 임시변수를 질의 함수로 바꾸기

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -1,4 +1,8 @@
 export default function statement(invoice, plays) {
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역(고객명 : ${invoice[0].customer})\n`;
@@ -9,7 +13,7 @@ export default function statement(invoice, plays) {
   }).format;
 
   for (const perf of invoice[0].performance) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다.
@@ -33,9 +37,6 @@ export default function statement(invoice, plays) {
   return result;
 }
 
-// statement함수 안에 밑에 코드가 있을 경우
-// 일일히 코드를 파악해야 하며, 이러한 코드는 휘발성이 높다
-// 그렇기 때문에 밑에 처럼 함수로 추출하여, 위에 코드에 반영해 가독성을 높인다.
 function amountFor(aPerformance, play) {
   let result = 0;
 
