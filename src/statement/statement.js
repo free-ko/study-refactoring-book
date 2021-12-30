74ce0dc50b22f71c611243a7c5a7d5ff2ebdfeeb
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-31 07:37:59 +0900

    [P.42~44] format함수 추출 및 이름 변경

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -38,26 +38,29 @@ export default function statement(invoice, plays) {
     return results;
   }
 
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역(고객명 : ${invoice[0].customer})\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
 
   for (const perf of invoice[0].performance) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
