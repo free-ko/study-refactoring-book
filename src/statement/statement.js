514ce6756826b55ec2751c4afb754b43d42373ec
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-05 07:38:58 +0900

    [P.55 ~ 57] 함수 옮기기 playFor, amountFor

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -1,9 +1,45 @@
 export default function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice[0].customer;
-  statementData.performances = invoice[0].performances;
+  statementData.performances = invoice[0].performances.map(enrichPerformance);
 
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -11,9 +47,8 @@ function renderPlainText(data, plays) {
 
   for (const perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
-      perf.audience
-    }석)\n`;
+    result += `${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience
+      }석)\n`;
   }
 
   result += `총액: ${usd(totalAmount())}\n`;
@@ -21,11 +56,24 @@ function renderPlainText(data, plays) {
 
   return result;
 
+  function volumeCreditsFor(aPerformance) {
+    let results = 0;
+    results += Math.max(aPerformance.audience - 30, 0);
+
+    if ("comedy" === aPerformance.play.type) {
+      results += Math.floor(aPerformance.audience / 5);
+    }
+
+    return results;
+  }
+
+
+
   function totalAmount() {
     let result = 0;
 
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -48,42 +96,6 @@ function renderPlainText(data, plays) {
     }).format(aNumber / 100);
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let results = 0;
-    results += Math.max(aPerformance.audience - 30, 0);
 
-    if ("comedy" === playFor(aPerformance).type) {
-      results += Math.floor(aPerformance.audience / 5);
-    }
 
-    return results;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-    }
-
-    return result;
-  }
 }
