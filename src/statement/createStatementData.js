b2e824d81b467393774affb8291d320cb478aea4
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-10 07:29:45 +0900

    [P.68~69] 클래스 내부로 함수 옮기기

--- src/statement/createStatementData.js
+++ src/statement/createStatementData.js
@@ -2,6 +2,41 @@ class PerformanceCalculator {
   constructor(aPerformance) {
     this.performance = aPerformance;
   }
+
+  // amountFor() 함수가 매개변수로 받던 정보를 계산기 필드에서 바로 얻음
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ("comedy" === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
+    }
+
+    return result
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -16,9 +51,10 @@ export default function createStatementData(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+
+    result.play = calculator.play
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
 
     return result;
   }
