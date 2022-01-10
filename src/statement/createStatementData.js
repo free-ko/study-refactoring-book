609dabe259706e2b6a2b76e851b056d8b22db8d7
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-11 07:25:20 +0900

    [P.71] 비극 공연료 계산 로직 추가

--- src/statement/createStatementData.js
+++ src/statement/createStatementData.js
@@ -1,9 +1,11 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-  switch (aPlay, type) {
-    case "tragedy": return new TragedyCalculator(aPerformance, aPlay)
-    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+  switch ((aPlay, type)) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
     default:
-      throw new Error(`알 수 없는 장르 : ${aPlay.type}`)
+      throw new Error(`알 수 없는 장르 : ${aPlay.type}`);
   }
 }
 
@@ -44,19 +46,23 @@ class PerformanceCalculator {
       result += Math.floor(this.performance.audience / 5);
     }
 
-    return result
+    return result;
   }
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
 
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+
+    return result;
+  }
 }
 
-class ComedyCalculator extends PerformanceCalculator {
-
-}
-
-
+class ComedyCalculator extends PerformanceCalculator { }
 
 export default function createStatementData(invoice, plays) {
   const result = {};
@@ -68,12 +74,15 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance);
 
-    result.play = calculator.play
-    result.amount = calculator.amount
-    result.volumeCredits = calculator.volumeCredits
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
 
     return result;
   }
@@ -124,6 +133,4 @@ export default function createStatementData(invoice, plays) {
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
-
 }
-
