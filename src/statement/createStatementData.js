5492b7c6a36948cc2e9de31ccaa1e22b2b06dbfc
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-12 07:30:28 +0900

    [P.74] 불필요한 로직 제거

--- src/statement/createStatementData.js
+++ src/statement/createStatementData.js
@@ -1,3 +1,39 @@
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice[0].customer;
+  result.performances = invoice[0].performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+
+  return result;
+
+  function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
+    const result = Object.assign({}, aPerformance);
+
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
+
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
   switch ((aPlay, type)) {
     case "tragedy":
@@ -50,75 +86,4 @@ class ComedyCalculator extends PerformanceCalculator {
     return super.volumeCredits + Math.floor(this.performance.audience / 5)
   }
 
-}
-
-export default function createStatementData(invoice, plays) {
-  const result = {};
-  result.customer = invoice[0].customer;
-  result.performances = invoice[0].performances.map(enrichPerformance);
-  result.totalAmount = totalAmount(result);
-  result.totalVolumeCredits = totalVolumeCredits(result);
-
-  return result;
-
-  function enrichPerformance(aPerformance) {
-    const calculator = createPerformanceCalculator(
-      aPerformance,
-      playFor(aPerformance)
-    );
-    const result = Object.assign({}, aPerformance);
-
-    result.play = calculator.play;
-    result.amount = calculator.amount;
-    result.volumeCredits = calculator.volumeCredits;
-
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let results = 0;
-    results += Math.max(aPerformance.audience - 30, 0);
-
-    if ("comedy" === aPerformance.play.type) {
-      results += Math.floor(aPerformance.audience / 5);
-    }
-
-    return results;
-  }
-
-  function totalAmount(data) {
-    return data.performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
 }
