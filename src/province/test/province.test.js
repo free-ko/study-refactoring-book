cccda2a6e74d34f181f87a4f410342b04e80f96b
kyw <47565280+free-ko@users.noreply.github.com>
2022-01-31 09:06:11 +0900

    [P.145] beforeEach 적용

--- src/province/test/province.test.js
+++ src/province/test/province.test.js
@@ -14,13 +14,17 @@ function sampleProvinceData() {
 }
 
 describe('province', function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  })
+
+
   it('shortfail', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5)
   })
 
   it('profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toEqual(230)
   })
 })
