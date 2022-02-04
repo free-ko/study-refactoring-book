981eaa15bcce0a54d272c31ed772414d19d055ad
kyw <47565280+free-ko@users.noreply.github.com>
2022-02-05 08:56:39 +0900

    [P.164-165] 지역변수 매개변수로 전달

--- src/refactoring/1.함수 추출하기/ex2.js
+++ src/refactoring/1.함수 추출하기/ex2.js
@@ -35,12 +35,12 @@ function printOwing(invoice) {
     outstanding += o.amount;
   }
 
-  // 마감일(dueDate)을 기록한다.
-  const today = Clock.today;
-  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+  // 마감일 설정 로직을 함수로 추출
+  recordDueDate(invoice)
 
   // 세부 사항 출력 로직을 함수로 추출
-  printDetails()
+  // 지역 변수를 매개변수로 전달
+  printDetails(invoice, outstanding)
 
   function printBanner() {
     console.log('**********')
@@ -49,10 +49,16 @@ function printOwing(invoice) {
 
   }
 
-  function printDetails() {
+  function printDetails(invoice, outstanding) {
     console.log(`고객명: ${invoice.customer}`)
     console.log(`채무액: ${outstanding}`)
     console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`)
+  }
+
+  function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+
   }
 }
 
