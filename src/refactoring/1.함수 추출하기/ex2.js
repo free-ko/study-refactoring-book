109b2eed07adb0012076a41783d6f8d7fee7921f
kyw <47565280+free-ko@users.noreply.github.com>
2022-02-05 09:01:09 +0900

    [P.168] 코드 정리

--- src/refactoring/1.함수 추출하기/ex2.js
+++ src/refactoring/1.함수 추출하기/ex2.js
@@ -25,40 +25,35 @@ function printOwing(invoice) {
 
 // 리팩토링 후
 function printOwing(invoice) {
-  let outstanding = 0;
-
-  // 배너 출력 로직을 함수로 추출
   printBanner()
-
-  // 미해결 채무(outstanding)을 계산한다.
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
-
-  // 마감일 설정 로직을 함수로 추출
+  const outstanding = calculateOutstanding(invoice)
   recordDueDate(invoice)
-
-  // 세부 사항 출력 로직을 함수로 추출
-  // 지역 변수를 매개변수로 전달
   printDetails(invoice, outstanding)
+}
 
-  function printBanner() {
-    console.log('**********')
-    console.log('***고객채무***')
-    console.log('**********')
+function printBanner() {
+  console.log('**********')
+  console.log('***고객채무***')
+  console.log('**********')
+}
 
+function printDetails(invoice, outstanding) {
+  console.log(`고객명: ${invoice.customer}`)
+  console.log(`채무액: ${outstanding}`)
+  console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`)
+}
+
+function recordDueDate(invoice) {
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
+}
+
+function calculateOutstanding(invoice) {
+  let result = 0;
+  for (const o of invoice.orders) {
+    result += o.amount;
   }
 
-  function printDetails(invoice, outstanding) {
-    console.log(`고객명: ${invoice.customer}`)
-    console.log(`채무액: ${outstanding}`)
-    console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`)
-  }
-
-  function recordDueDate(invoice) {
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30)
-
-  }
+  return result
 }
 
