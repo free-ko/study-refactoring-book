71aeac05933b83154b4e2728b43b377fc3c10cfe
kyw <47565280+free-ko@users.noreply.github.com>
2022-02-11 12:13:38 +0900

    [P.187] 함수선언 리팩토링 코드

--- src/refactoring/4.함수선언 바꾸기/ex1.js
+++ src/refactoring/4.함수선언 바꾸기/ex1.js
@@ -1,3 +1,16 @@
 function inNewEngland(aCustomer) {
   return ["MA", "CT", "ME", "VI", "NH", "RI"].includes(aCustomer.address.state);
 }
+
+// 호출
+const newEnglanders = somCustomers.filter((c) => inNewEngland(c));
+
+
+
+// 리팩토링 후
+function inNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VI", "NH", "RI"].includes(stateCode);
+}
+
+// 호출
+const newEnglanders = somCustomers.filter((c) => inNewEngland(c.address.state));
