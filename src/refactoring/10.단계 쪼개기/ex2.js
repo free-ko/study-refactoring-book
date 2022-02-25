cde6e563a363d73f66a3bc841df05a868c24a67f
kyw <47565280+free-ko@users.noreply.github.com>
2022-02-25 17:30:11 +0900

    [P.216] 배송비 계산 부분 함수 추출

--- src/refactoring/10.단계 쪼개기/ex2.js
+++ src/refactoring/10.단계 쪼개기/ex2.js
@@ -13,3 +13,26 @@ function priceOrder(product, quantity, shippingMethod) {
   const price = basePrice - discount + shippingCost;
   return price;
 }
+
+// 함수 추출
+function priceOrder(product, quantity, shippingMethod) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount)
+
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price
+}
