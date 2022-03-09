881e50e9594839294e10e4007174b60762c781a4
kyw <47565280+free-ko@users.noreply.github.com>
2022-03-10 08:17:29 +0900

    [P.267] 클래스 인라인 하기 학습

---
+++ src/refactoring/11.캡슐화/06.클래스 인라인하기/ex1.js
@@ -0,0 +1,24 @@
+class Shipement {
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`
+  }
+
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this._shippingCompany = arg
+  }
+
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+}
+
+// 클라이언트
+aShipment.shippingCompany = request.vendor;
