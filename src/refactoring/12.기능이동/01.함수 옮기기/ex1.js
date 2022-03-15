b513edc843f901cf5940ec1fba5d8cbcb0dc478b
kyw <47565280+free-ko@users.noreply.github.com>
2022-03-16 07:30:15 +0900

    [P.285] 함수 옮기기 예제 학습

--- src/refactoring/12.기능이동/01.함수 옮기기/ex1.js
+++ src/refactoring/12.기능이동/01.함수 옮기기/ex1.js
@@ -1,42 +1,36 @@
 // 중첩 함수
 function trackSummary(points) {
   const tatalTime = calculateItme();
-  const totalDistance = calculateDistance();
-  const pace = tatalTime / 60 / totalDistance;
+  const pace = tatalTime / 60 / totalDistance(points);
 
   return {
     titme: tatalTime,
-    distance: totalDistance,
+    distance: totalDistance(points),
     pace: pace
-  }
-
-  function calculateDistance() {
-    return top_calculateDistance(points);
-  }
-
+  };
 }
 
-function top_calculateDistance(points) {
+function totalDistance(points) {
   let result = 0;
   for (let i = 0; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
   }
   return result
+}
 
-  function distance(p1, p2) {
-    const EARTH_RADIUS = 3959; // 단위: 마일(mile)
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a = Math.pow(Math.sin(dLat / 2), 2)
-      + Math.cos(radians(p2.lat))
-      + Math.cos(radians(p1.lat))
-      + Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+function distance(p1, p2) {
+  const EARTH_RADIUS = 3959; // 단위: 마일(mile)
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a = Math.pow(Math.sin(dLat / 2), 2)
+    + Math.cos(radians(p2.lat))
+    + Math.cos(radians(p1.lat))
+    + Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    return EARTH_RADIUS * c;
-  }
+  return EARTH_RADIUS * c;
+}
 
-  function radians(degrees) {
-    return degrees * Math.PI / 100;
-  }
+function radians(degrees) {
+  return degrees * Math.PI / 100;
 }
