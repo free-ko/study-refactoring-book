d23fcb0d0ee1383d2251a67ee6137e703d84881c
free-ko <duddnr654@naver.com>
2022-03-22 07:30:00 +0900

    [P.300] 문장을 함수로 옮기기 예제 학습

---
+++ src/refactoring/12.기능이동/03.문장을 함수로 옮기기/ex1.js
@@ -0,0 +1,25 @@
+function renderPerson(outStream, person) {
+  const result = [];
+  result.push(`<p>${person}</p>`);
+  result.push(renderPhoto(person.photo));
+  result.push(emitPhotoData(person.photo));
+  result.push(emitPhotoData(person.photo));
+
+  return result.join("\n")
+};
+
+function photoDiv(aPhoto) {
+  return [
+    "<div>",
+    emitPhotoData(aPhoto),
+    "</div>",
+  ].join("\n")
+};
+
+function emitPhotoData(aPhoto) {
+  return [
+    `<p>제목: ${aPhoto.title}</p>`,
+    `<p>위치: ${aPhoto.location}</p>`,
+    `<p>날짜: ${aPhoto.date.toDateString()}</p>`
+  ].join("\n")
+};
