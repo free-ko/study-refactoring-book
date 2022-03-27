2dee7c00db5a1ad8046fbcf74d6b66e47129f7ce
free-ko <duddnr654@naver.com>
2022-03-27 10:22:03 +0900

    [P.332] 변수 쪼개기 학습 중

---
+++ src/refactoring/13.데이터 조직화/01.변수 쪼개기/ex.js
@@ -0,0 +1,15 @@
+function distanceTravelled(scenario, time) {
+  let result;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass;  // 가속도(a) = 힘(F) / 질량(m)
+  let primaryTime = Math.min(time, scenario.delay);
+  result = 0.5 * primaryAcceleration * primaryTime;  // 전파된 거리
+
+  let secondaryTime = time - scenario.delay;
+  if (secondaryTime > 0) {  // 두 번째 힘을 반영해 다시 계산
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    let acc = (scenario.primaryForce + scenario.secondaryTime) / scenario.mass;
+    result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime
+  }
+
+  return result;
+}
