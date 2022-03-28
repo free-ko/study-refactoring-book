d1e233a27b9c57cd699a3fbed7749be566266130
free-ko <duddnr654@naver.com>
2022-03-28 11:53:42 +0900

    [P.332] 변수 쪼개기 학습

--- src/refactoring/13.데이터 조직화/01.변수 쪼개기/ex.js
+++ src/refactoring/13.데이터 조직화/01.변수 쪼개기/ex.js
@@ -7,9 +7,10 @@ function distanceTravelled(scenario, time) {
   let secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {  // 두 번째 힘을 반영해 다시 계산
     let primaryVelocity = primaryAcceleration * scenario.delay;
-    let acc = (scenario.primaryForce + scenario.secondaryTime) / scenario.mass;
-    result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime
+    const secondaryAcceleration = (scenario.primaryForce + scenario.secondaryTime) / scenario.mass;
+    result += primaryVelocity * secondaryTime + 0.5 * secondaryAcceleration * secondaryTime * secondaryTime
   }
 
   return result;
 }
+
