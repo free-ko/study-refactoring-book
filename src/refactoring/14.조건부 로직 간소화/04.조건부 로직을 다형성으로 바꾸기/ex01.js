5f33cbcdf9b7cf9d56840d17c4ca9159cd541fda
free-ko <duddnr654@naver.com>
2022-04-08 07:31:06 +0900

    [P.374] 예제 학습 중

--- src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex01.js
+++ src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex01.js
@@ -1,11 +1,3 @@
-function plumages(bird) {
-  return new Bird(bird).plumage;
-}
-
-function speeds(bird) {
-  return new Bird(bird).airSpeedVelocity;
-}
-
 class Bird {
   constructor(birdObject) {
     Object.assign(this, birdObject);
@@ -16,43 +8,16 @@ class Bird {
   }
 
   get airSpeedVelocity() {
-    switch (this.type) {
-      case '유럽 제비':
-        return 35;
-      case '아프리카 제비':
-        return 40 - 2 * this.numberOfCounts;
-      case '노르웨이 파랑 앵무':
-        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }
-
-function plumage(bird) {
-  return createBird(bird).plumage;
-}
-
-function airSpeedVelocity(bird) {
-  return createBird(bird).airSpeedVelocity;
-}
-
-function createBird(bird) {
-  switch(bird.type) {
-    case '유럽 제비':
-      return new EuropeanSwallow(bird);
-    case '아프리카 제비':
-      return new AfricanSwallow(bird);
-    case '노르웨이 파랑 앵무':
-      return new NorwegianBlueParrot(bird);
-    default:
-      return new Bird(bird);
-  }
-}
-
 class EuropeanSwallow extends Bird {
   get plumage() {
     return '보통이다';
+  }
+
+  get airSpeedVelocity() {
+    return 35;
   }
 }
 
@@ -60,10 +25,18 @@ class AfricanSwallow extends Bird {
   get plumage() {
     return (this.numberOfCounts > 2) ? '지쳤다' : '보통이다';
   }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCounts;
+  }
 }
 
 class NorwegianBlueParrot extends Bird {
   get plumage() {
     return (this.voltage > 100) ? '그을렸다' : '예쁘다';
+  }
+
+  get airSpeedVelocity() {
+    return (this.isNailed) ? 0 : 10 + this.voltage / 10;
   }
 }
