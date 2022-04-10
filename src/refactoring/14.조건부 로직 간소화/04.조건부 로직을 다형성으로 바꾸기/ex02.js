9632ce187b2cbf9fbbedb71e63298122889f473b
free-ko <duddnr654@naver.com>
2022-04-11 07:31:38 +0900

    [P.378] 조건부 로직 간소화 예제 학습 중

--- src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
+++ src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
@@ -34,13 +34,13 @@ function captainHistoryRisk(voyage, history) {
   let result = 1;
 
   if (history.length < 5) {
-    return result += 4;
+    result += 4;
   }
 
   result += history.filter(v => v.profit < 0).length;
 
   if (voyage.zone === '중국' && hasChina(history)) {
-    return result -= 2;
+    result -= 2;
   }
 
   return Math.max(result, 0);
@@ -53,4 +53,65 @@ function hasChina(history) { // 중국을 경유하는가?
   // Includes는 배열안에 특정 객체 속성 값 포함 여부 확인 불가
   // 왜냐하면 Includes === 연산자로 객체 원시값만 비교함
   return history.some(v => "중국" === v.zone);
+}
+
+function voyageProfitFactor(voyage, history) { // 수익 요인
+  let result = 2;
+
+  if (voyage.zone === "중국") {
+    return result += 1;
+  }
+
+  if (voyage.zone === '동인도') {
+    return result += 1;
+  }
+
+  if (voyage.zone === "중국" && hasChina(history)) {
+    result += 3;
+
+    if (history.length > 10) {
+      result += 1;
+    }
+
+    if (history.length > 12) {
+      result += 1;
+    }
+
+    if (history.length > 18) {
+      result -= 1;
+    }
+  } else {
+    if (history.length > 8) {
+      result += 1;
+
+    }
+
+    if (voyage.length > 14) {
+      result -= 1;
+    }
+  }
+
+  return result;
+}
+
+const voyage = { zone: "서인도", length: 10 };
+const history = [
+  { zone: "동인도", profit: 5 },
+  { zone: "서인동", profit: 15 },
+  { zone: "중국", profit: -2 },
+  { zone: "서아프리카", profit: 7 },
+];
+
+
+// 클래스로 리팩토링
+function rating(voyage, history) {
+  return new Rating(voyage, history).value;
+}
+
+class Rating { // 함수를 Rating 클래스로 묶음
+  constructor(voyage, history) {
+    this.voyage = voyage;
+    this.history = history;
+  }
+
 }
