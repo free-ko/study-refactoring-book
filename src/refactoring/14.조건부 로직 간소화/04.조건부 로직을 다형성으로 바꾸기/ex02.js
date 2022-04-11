afc2c3f7a28bded70099365acf31295a75839985
free-ko <duddnr654@naver.com>
2022-04-12 07:30:43 +0900

    [P.380] 조건부 로직 다형성 바꾸기 예제 학습 중

--- src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
+++ src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
@@ -47,11 +47,6 @@ function captainHistoryRisk(voyage, history) {
 }
 
 function hasChina(history) { // 중국을 경유하는가?
-
-  // Some과 Includes 함수 차이
-  // Some은 배열 안의 객체 내부 값을 비교 가능(특정 콜백함수 포함시켜서)
-  // Includes는 배열안에 특정 객체 속성 값 포함 여부 확인 불가
-  // 왜냐하면 Includes === 연산자로 객체 원시값만 비교함
   return history.some(v => "중국" === v.zone);
 }
 
@@ -105,7 +100,14 @@ const history = [
 
 // 클래스로 리팩토링
 function rating(voyage, history) {
-  return new Rating(voyage, history).value;
+  return createRating(voyage, history).value;
+}
+
+function createRating(voyage, history) {
+  if (voyage.zone === "중국" && history.some(v => "중국" === v.zone)) {
+    return new ExperincedChinaRating(voyage, history);
+  }
+  return new Rating(voyage, history);
 }
 
 class Rating { // 함수를 Rating 클래스로 묶음
@@ -114,4 +116,96 @@ class Rating { // 함수를 Rating 클래스로 묶음
     this.history = history;
   }
 
+  get value() {
+    const vpf = voyageProfitFactor(voyage, history);
+    const vr = voyageRisk(voyage);
+    const chr = captainHistoryRisk(voyage, history);
+
+    if (vpf * 3 > (vr + chr * 2)) {
+      return 'A';
+    } else {
+      return 'B';
+    }
+  }
+
+  get voyageRisk() {
+    let result = 1;
+
+    if (voyage.length > 4) {
+      return result += 2;
+    }
+
+    if (voyage.length > 8) {
+      return result += voyage.length - 8;
+    }
+
+    if (["중국", "동인도"].includes(voyage.zone)) {
+      return result += 4;
+    }
+
+    return Math.max(result, 0);
+  }
+
+  get captainHistoryRisk() {
+    let result = 1;
+
+    if (history.length < 5) {
+      result += 4;
+    }
+
+    result += history.filter(v => v.profit < 0).length;
+
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    let result = 2;
+
+    if (voyage.zone === "중국") {
+      return result += 1;
+    }
+
+    if (voyage.zone === '동인도') {
+      return result += 1;
+    }
+
+    if (this.voyage.zone === "중국" && this.hasChinaHistory) {
+      result += 3;
+
+      if (this.history.length > 10) {
+        result += 1;
+      }
+
+      if (this.history.length > 12) {
+        result += 1;
+      }
+
+      if (this.history.length > 18) {
+        result -= 1;
+      }
+    } else {
+      if (this.history.length > 8) {
+        result += 1;
+
+      }
+
+      if (this.voyage.length > 14) {
+        result -= 1;
+      }
+    }
+
+    return result;
+  }
+
+  get hasChina() {
+    return history.some(v => "중국" === v.zone);
+  }
 }
+
+class ExperincedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+    return Math.max(result, 0);
+  }
+}
+
