bf3c2b6dd9202b443e3818e41a95db90b2b25df6
free-ko <duddnr654@naver.com>
2022-04-14 07:33:20 +0900

    [P.387] 조건부 로직 간소화 예제 학습

--- src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
+++ src/refactoring/14.조건부 로직 간소화/04.조건부 로직을 다형성으로 바꾸기/ex02.js
@@ -6,16 +6,16 @@ const history = [
   { zone: "중국", profit: -2 },
   { zone: "서아프리카", profit: 7 },
 ];
-class Rating { // 함수를 Rating 클래스로 묶음
+class Rating {
   constructor(voyage, history) {
     this.voyage = voyage;
     this.history = history;
   }
 
   get value() {
-    const vpf = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
 
     if (vpf * 3 > (vr + chr * 2)) {
       return 'A';
@@ -27,17 +27,9 @@ class Rating { // 함수를 Rating 클래스로 묶음
   get voyageRisk() {
     let result = 1;
 
-    if (voyage.length > 4) {
-      return result += 2;
-    }
-
-    if (voyage.length > 8) {
-      return result += voyage.length - 8;
-    }
-
-    if (["중국", "동인도"].includes(voyage.zone)) {
-      return result += 4;
-    }
+    if (voyage.length > 4) result += 2;
+    if (voyage.length > 8) result += voyage.length - 8;
+    if (["중국", "동인도"].includes(voyage.zone)) result += 4;
 
     return Math.max(result, 0);
   }
@@ -45,9 +37,7 @@ class Rating { // 함수를 Rating 클래스로 묶음
   get captainHistoryRisk() {
     let result = 1;
 
-    if (history.length < 5) {
-      result += 4;
-    }
+    if (history.length < 5) result += 4;
 
     result += history.filter(v => v.profit < 0).length;
 
@@ -57,16 +47,12 @@ class Rating { // 함수를 Rating 클래스로 묶음
   get voyageProfitFactor() {
     let result = 2;
 
-    if (voyage.zone === "중국") {
-      return result += 1;
-    }
-
-    if (voyage.zone === '동인도') {
-      return result += 1;
-    }
+    if (voyage.zone === "중국") result += 1;
+    if (voyage.zone === '동인도') result += 1;
 
     result += this.historyLengthFactor;
     result += this.voyageLengthFactor;
+
     return result;
   }
 
@@ -74,26 +60,23 @@ class Rating { // 함수를 Rating 클래스로 묶음
     return (this.voyage.length > 14) ? -1 : 0;
   }
 
-  get voyageAndHistoryLengthFactor() {
-    let result = 0;
-
-    if (this.voyage.length > 14) result -= 1;
-
-    return result;
-  }
-
   get historyLengthFactor() {
     return (this.history.length > 8) ? 1 : 0;
   }
-
-  get hasChina() {
-    return history.some(v => "중국" === v.zone);
-  }
 }
 class ExperincedChinaRating extends Rating {
+  get captainHistoryRisk() {
+    const result = super.captainHistoryRisk - 2;
+
+    return Math.max(result, 0);
+  }
+
+  get voyageLengthFactor() {
+    return super.voyageProfitFactor + 3;
+  }
+
   get voyageLengthFactor() {
     let result = 0;
-    result += 3;
 
     if (this.voyage.length > 12) result += 1;
     if (this.voyage.length > 18) result -= 1;
@@ -103,6 +86,10 @@ class ExperincedChinaRating extends Rating {
 
   get historyLengthFactor() {
     return (this.history.length > 10) ? 1 : 0;
+  }
+
+  get voyageProfitFactor() {
+    return super.voyageProfitFactor + 3;
   }
 }
 
