2808b64efe73df10daaeca95c9799af0445bdb7f
free-ko <duddnr654@naver.com>
2022-04-24 11:17:36 +0900

    [P.424] 플래그 인수 제거하기 예제 학습중

--- src/refactoring/15.API 리팩토링/03. 플래그 인수 제거하기/ex01.js
+++ src/refactoring/15.API 리팩토링/03. 플래그 인수 제거하기/ex01.js
@@ -1,23 +1,52 @@
-function deliveryDate(anOrder, isRush) {
-  if (isRush) {
-    let deliveryTime;
+// function deliveryDate(anOrder, isRush) {
+//   if (isRush) {
+//     let deliveryTime;
 
-    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else deliveryTime = 3;
+//     if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+//     else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+//     else deliveryTime = 3;
 
-    return anOrder.placeOn.plusDays(1 + deliveryTime);
-  } else {
-    let deliveryTime;
+//     return anOrder.placeOn.plusDays(1 + deliveryTime);
+//   } else {
+//     let deliveryTime;
 
-    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 2;
-    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-    else deliveryTime = 4;
+//     if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 2;
+//     else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+//     else deliveryTime = 4;
 
-    return anOrder.placeOn.plusDays(2 + deliveryTime);
-  }
-}
+//     return anOrder.placeOn.plusDays(2 + deliveryTime);
+//   }
+// }
 
 // 호출
 // 2번째 인자 true가 무엇을 의미하는지 모르는 문제 발생(플래그 인수)
-aShipment.deliveryDate = deliveryDate(anOrder, true);   
+aShipment.deliveryDate = deliveryDate(anOrder, true);
+
+// 수정
+function deliveryDate(anOrder, isRush) {
+  if (isRush) return rushDeliveryDate(anOrder);
+
+  return regularDeliveryDate(anOrder);
+}
+
+function rushDeliveryDate(anOrder) {
+  let deliveryTime;
+
+  if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+  else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else deliveryTime = 3;
+
+  return anOrder.placeOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+  let deliveryTime;
+
+  if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 2;
+  else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+  else deliveryTime = 4;
+
+  return anOrder.placeOn.plusDays(2 + deliveryTime);
+}
+
+aShipment.deliveryDate = rushDeliveryDate(anOrder);  
