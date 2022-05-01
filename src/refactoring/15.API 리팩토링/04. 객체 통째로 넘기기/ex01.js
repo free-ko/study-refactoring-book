2bf456fe77dbb31914a477276cd15a861322b5dc
free-ko <duddnr654@naver.com>
2022-05-01 10:30:25 +0900

    [P.432] 객체 통째로 넘기기 예제 학습중

--- src/refactoring/15.API 리팩토링/04. 객체 통째로 넘기기/ex01.js
+++ src/refactoring/15.API 리팩토링/04. 객체 통째로 넘기기/ex01.js
@@ -8,3 +8,16 @@ class HeatingPlan {
 
 // 호출자
 if (!aPlan.withinRange(aRoom.daysTempRange)) alerts.push('방 온도 지정 범위를 벗어났습니다.');
+
+
+// 다른 경우
+const tempRange = aRoom.daysTempRange;
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
+if (!isWithinRange) alerts.push('방 온도 지정 범위를 벗어났습니다.');
+
+function xxNEWwithinRange(tempRange) {
+  const low = tempRange.low;
+  const high = tempRange.high;
+  const isWithinRange = this.withinRange(low, high);
+  return isWithinRange;
+}
