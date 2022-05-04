637956bbd4775318495232c0d5219ceee2d04623
free-ko <duddnr654@naver.com>
2022-05-04 09:37:21 +0900

    추출한 변수를 인라인 적용

--- src/refactoring/15.API 리팩토링/06.질의 함수를 매개변수로 바꾸기/ex01.js
+++ src/refactoring/15.API 리팩토링/06.질의 함수를 매개변수로 바꾸기/ex01.js
@@ -2,7 +2,7 @@ class HeatingPlan {
   get targetTemperature() {
     const selectedTemperature = thermostat.selectedTemperature;
 
-    return this.xxNEWtargetTemperature(selectedTemperature);
+    return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
   }
 
   xxNEWtargetTemperature(selectedTemperature) {
