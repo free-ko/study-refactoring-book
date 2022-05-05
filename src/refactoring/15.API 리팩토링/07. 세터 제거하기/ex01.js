b2e4a8cc934cb5a83e1ce04c811d1c941d9871bf
free-ko <duddnr654@naver.com>
2022-05-05 12:24:50 +0900

    [P.444] set 메서드 제거

--- src/refactoring/15.API 리팩토링/07. 세터 제거하기/ex01.js
+++ src/refactoring/15.API 리팩토링/07. 세터 제거하기/ex01.js
@@ -8,16 +8,8 @@ class Person {
     return this._name;
   }
 
-  set name(arg) {
-    this._name = arg;
-  }
-
   get id() {
     return this._id;
-  }
-
-  set id(arg) {
-    return this._id = arg;
   }
 }
 
