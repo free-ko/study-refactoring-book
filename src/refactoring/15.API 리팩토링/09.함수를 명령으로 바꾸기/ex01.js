8a06c50293b912cdffc49a2b5145ba102e88fed9
free-ko <duddnr654@naver.com>
2022-05-10 09:29:50 +0900

    [P.455] 명령을 중첩함수처럼 사용

--- src/refactoring/15.API 리팩토링/09.함수를 명령으로 바꾸기/ex01.js
+++ src/refactoring/15.API 리팩토링/09.함수를 명령으로 바꾸기/ex01.js
@@ -1,20 +1,41 @@
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer(candidate, medicalExam, scoringGuide).excute();
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+class Scorer {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+    this._scoringGuide = scoringGuide;
   }
 
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
+  excute() {
+    this._result = 0;
+    this._healthLevel = 0;
+    this._highMedicalRiskFlag = false;
+
+    this.scroeSmoking();
+    this._certificationGrade = "regular";
+
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
+  
+    if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
+      this._certificationGrade = "low";
+      this._result -= 5;
+    }
+  
+    // 비슷한 코드가 한참 이어짐
+    this._result -= Math.max(this._healthLevel - 5, 0);
+    return this._result; 
   }
 
-  // 비슷한 코드가 한참 이어짐
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
+  scroeSmoking() {
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
+  }
 }
