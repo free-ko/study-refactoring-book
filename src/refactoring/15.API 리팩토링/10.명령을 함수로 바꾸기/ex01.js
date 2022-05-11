54499f072717eef88e2f832465a8a4e6f890a9ee
free-ko <duddnr654@naver.com>
2022-05-11 09:23:12 +0900

    [P.460] 명령을 함수로 바꾸기 예제 학습

--- src/refactoring/15.API 리팩토링/10.명령을 함수로 바꾸기/ex01.js
+++ src/refactoring/15.API 리팩토링/10.명령을 함수로 바꾸기/ex01.js
@@ -1,18 +1,6 @@
-class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
-
 // 호출자
-const monthCharge = new ChargeCalculator(customer, usage, provider).charge;
+const mothCharge = charge(customer, usage, provider);
