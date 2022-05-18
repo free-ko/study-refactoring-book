e4856aa13efbd7c72ba2ccb4610548958a5debb1
free-ko <duddnr654@naver.com>
2022-05-18 09:33:51 +0900

    [P.470] 오류 코드를 예외 코드로 바꾸기 예제 학습

--- src/refactoring/15.API 리팩토링/12.오류 코드를 예외로 바꾸기/ex01.js
+++ src/refactoring/15.API 리팩토링/12.오류 코드를 예외로 바꾸기/ex01.js
@@ -1,43 +1,26 @@
 function localShippingRules(country) {
-  const data = countryData.shippingRules[country];
-  if (data) {
-    return new ShippingRules(data);
-  } else {
-    throw new OrderProcessingError(-23);
-  }
-
-  let errorList;
-  let status;
-  try {
-    status = calculateShippingCosts(orderData);
-  } catch (e) {
-    if (e instanceof OrderProcessingError) {
-      errorList.push({ order: orderData, errorCode: e.code });
+    const data = countryData.shippingRules[country];
+    if (data) {
+        return new ShippingRules(data);
     } else {
-      throw e;
+        throw new OrderProcessingError(-23);
     }
-  }
-
-  if (status < 0) errorList.push({ order: orderData, errorCode: status });
 }
 
 function calculateShippingCosts(anOrder) {
-  // 관련없는 코드
-  const shippingRules = localShippingRules(anOrder.country);
-  if (shippingRules < 0) return shippingRules; // 오류 전파
+    // 관련없는 코드
+    const shippingRules = localShippingRules(anOrder.country);
 
-  // 더 관련없는 코드
-
-  return "Status";
+    // 더 관련없는 코드
 }
 
 class OrderProcessingError extends Error {
-  constructor(errorCode) {
-    super(`주문 처리 오류: ${errorCode}`);
-    this.code = errorCode;
-  }
+    constructor(errorCode) {
+        super(`주문 처리 오류: ${errorCode}`);
+        this.code = errorCode;
+    }
 
-  get name() {
-    return "OrderProcessingError";
-  }
+    get name() {
+        return "OrderProcessingError";
+    }
 }
