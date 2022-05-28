b5c944fd81e96db171b4909b6b24dec5f0e32471
free-ko <duddnr654@naver.com>
2022-05-28 11:45:02 +0900

    [494]: 직원 유형 리팩토링

--- src/refactoring/16.상속 다루기/03.타입 코드를 서브 클래스로 바꾸기/ex02.js
+++ src/refactoring/16.상속 다루기/03.타입 코드를 서브 클래스로 바꾸기/ex02.js
@@ -11,8 +11,18 @@ class Employee {
     }
   }
   get typeString() { return this._type.toString(); }
+
   get type() { return this._type; }
-  set type(arg) { this._type = arg; }
+  set type(arg) { this._type = Employee.createEmployeeType(arg); }
+
+  static createEmployeeType(aString) {
+    switch (aString) {
+      case "engineer": return new Engineer();
+      case "manager": return new Manager();
+      case "salesperson": return new Salesperson();
+      default: throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+  }
 
   get capitalizedType() {
     return this.typeString.charAt(0).toUpperCase()
@@ -20,14 +30,31 @@ class Employee {
   }
 
   toString() {
-    return `${this._name} (${this.capitalizedType})`;
+    return `${this._name} (${this.type.capitalizedName})`;
   }
 }
 
-class EmplyeeType {
+class EmployeeType {
   constructor(aString) {
     this._value = aString;
   }
 
   toString() { return this._value; }
+
+  get capitalizedName() {
+    return this.toString().chartAt(0).toUpperCase()
+      + this.toString().substr(1).toLowerCase();
+  }
+}
+
+class Engineer extends EmployeeType {
+  toString() { return "engineer"; }
+}
+
+class Manager extends EmployeeType {
+  toString() { return "manager"; }
+}
+
+class Salesperson extends EmployeeType {
+  toString() { return "salesperson"; }
 }
