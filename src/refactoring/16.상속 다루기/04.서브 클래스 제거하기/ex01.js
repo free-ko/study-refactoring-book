ecc99d03b4fd599d968f1c8f829ed2841eeb6ac0
free-ko <duddnr654@naver.com>
2022-05-31 09:52:39 +0900

    [P.500] 서브 클래스 제거하기 예제 학습

--- src/refactoring/16.상속 다루기/04.서브 클래스 제거하기/ex01.js
+++ src/refactoring/16.상속 다루기/04.서브 클래스 제거하기/ex01.js
@@ -1,28 +1,21 @@
 class Person {
-  constructor(name) {
+  constructor(name, genderCode) {
     this._name = name;
+    this._genderCode = genderCode;
   }
 
   get name() { return this._name; }
-  get genderCode() { return "X"; }
-  // 생략
-}
-
-class Male extends Person {
-  get genderCode() { return "M"; }
-}
-
-class Female extends Person {
-  get genderCode() { return "F"; }
+  get genderCode() { return this._genderCode; }
+  get isMale() { return "M" === this._genderCode; }
 }
 
 const numberOfMales = people.filter(p => p.isMale).length;
 
 function createPerson(aRecord) {
   switch (aRecord.gender) {
-    case 'M': return new Male(aRecord.name); break;
-    case 'F': return new Female(aRecord.name); break;
-    default: return new Person(aRecord.name);
+    case 'M': return new Person(aRecord.name, "M");
+    case 'F': return new Person(aRecord.name, "F");
+    default: return new Person(aRecord.name, "X");
   }
 }
 
